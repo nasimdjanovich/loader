ebd148be8d525378558edbe2518f7c746191f2a0
Karimjon Komilov <102401252+nasimdjanovich@users.noreply.github.com>
2022-04-14 12:46:13 +0500

    files

---
+++ app.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const loader = document.querySelector('.loader');
+    setTimeout(() => {
+        loader.style.opacity = '0';
+        setTimeout(() => {
+            loader.style.display = 'none';
+        }, 500);
+    }, 2000);
+});
